9440692b01e12d0c775ae4e2fb222c4063dc53b3
TOÑO AGUIRRE <caaz160782@gmail.com>
2023-03-13 17:36:29 -0600

    instalando tailwindcss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
-function Header(){
-    return(
-        <>Desde Header</>
-    )
-}
+import React from 'react'
 
-export default Header;
+const Header = () => {
+  return (
+    <header className='grid'>
+      <h1 className="text-indigo-600 mt-5">Desde header</h1>
+    </header>
+  )
+}
+export default Header
